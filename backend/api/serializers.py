3087b66e6355030da4e431a0ca52371a5e834a8a
Douglous Sobei <douglousmangoyi@gmail.com>
2025-09-17 22:13:11 +0300

    Login Serializers and Login views

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,5 +1,23 @@
 from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
 from userauths.models import Profile, User
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    """
+    Custom token serializer to include additional user information in the JWT token.
+    """
+
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        token['full_name'] = user.full_name
+        token['email'] = user.email
+        token['username'] = user.username
+
+        return token
 
 
 class UserSerializer(serializers.ModelSerializer):
---
+++ backend/api/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from api import views as api_views
+
+urlpatterns = [
+    path('user/token/', api_views.MyTokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
+]
